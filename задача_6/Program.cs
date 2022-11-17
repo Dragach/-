a4415a701edca76118b96b15fae04a7815324875
Dragach <dragach@MacBook-Pro-user1.local>
2022-11-17 23:02:12 +0300

    задача 6 готова

---
+++ задача_6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Эта прорамма узнает четное ли введенное число или нет.");
+Console.WriteLine("Введите целое число:");
+
+string? a = Console.ReadLine ( );
+
+int intValue1 = Convert.ToInt32( a );
+
+if( intValue1 % 2 ==0 ) Console.WriteLine("Четное");
+else Console.WriteLine("Нечетное");
+
