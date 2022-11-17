5fc929bf63428c661b59872f42a7cb57d5e2d169
Dragach <dragach@MacBook-Pro-user1.local>
2022-11-17 22:28:44 +0300

    задача 2 готова

--- задача_2/Program.cs
+++ задача_2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Эта программа находит большее число из двух.");
+Console.WriteLine ("Введите первое число:");
+string? A = Console.ReadLine ( );
+Console.WriteLine ("Введите второе число:");
+string? B = Console.ReadLine ( );
+
+int intValue1 = Convert.ToInt32 ( A );
+int intValue2 = Convert.ToInt32 ( B );
+
+int max = intValue1;
+
+if ( max < intValue2 )
+{
+    max = intValue2;
+    
+    Console.Write ("Ответ:");
+    Console.WriteLine ( max );
+}
+else
+
+Console.WriteLine ("Ответ:");
+Console.WriteLine (max);
