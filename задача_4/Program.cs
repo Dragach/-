2fb4aa45f0b93d8bd29202bc50b66c1a7676cd64
Dragach <dragach@MacBook-Pro-user1.local>
2022-11-17 22:48:14 +0300

    задача 4 готова

---
+++ задача_4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Эта прорамма находит максимальное из трех чисел.");
+Console.WriteLine("Введите 3 целых числа:");
+
+string? a = Console.ReadLine();
+string? b = Console.ReadLine();
+string? c = Console.ReadLine();
+
+
+int intValue1 = Convert.ToInt32(a);
+int intValue2 = Convert.ToInt32(b);
+int intValue3 = Convert.ToInt32(c);
+
+int max = intValue1;
+
+if ( intValue2 > max ) max = intValue2;
+if ( intValue3 > max ) max = intValue3;
+
+Console.WriteLine("Cамое большое число из списка:");
+Console.WriteLine(max);
