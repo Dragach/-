6e410ebb075a84fc75e103bc227f722ec097bea1
Dragach <dragach@MacBook-Pro-user1.local>
2022-11-20 01:59:42 +0300

    првая часть задачи 10 готова

---
+++ задча 10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Эта программа показывает цыфру из разряда десятков трехзначного числа");
+Console.WriteLine("Введите целое трехзначное число:");
+
+string? a = Console.ReadLine();
+int b = Convert.ToInt32(a);
+
+if (b>99)
+{
+    //основной код
+}
+else
+{
+    Console.WriteLine("Ошибка 1: число не соответсвует условиям.");
+    Console.WriteLine(" ");
+    Console.WriteLine("Введите ТРЕХЗНАЧНОЕ число!");
+}
