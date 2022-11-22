876babaf8fdb5dae2f17924e4555d2e16dd69e48
Dragach <dragach@MacBook-Pro-user1.local>
2022-11-22 20:48:16 +0300

    исправил названия папок

---
+++ задача_13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Эта прорамма генерирует случайное число в диапазоне от 1 до 100 000 включительно и выводит третью цифру заданного числа ");
+
+int number = new Random().Next( 1, 100001 );
+string strNumbers = number.ToString();
+Console.WriteLine( strNumbers );
+
+if ( strNumbers.Length < 3 )
+{
+    Console.WriteLine("3-го числа нет");
+}
+else
+{
+   Console.WriteLine("Третья цифрв заданного чилса:");
+    Console.WriteLine(strNumbers[2]);
+}
