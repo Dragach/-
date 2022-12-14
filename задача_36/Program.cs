5150af0e20c8fdfb87f66692d94eb30b9ff223fb
Dragach <dragach@MacBook-Pro-user1.local>
2022-12-14 20:43:18 +0700

    задача 36 готова

---
+++ задача_36/Program.cs
@@ -0,0 +1,39 @@
+﻿//функция которая заполняет массив рандомными числами от -10 до 10
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while ( index < lenght )
+    {
+        collection[index] = new Random().Next ( -10, 11 );
+        index++;
+    }
+}
+// функция которая выводит все элементы массива 
+void PrintArray( int[] col )
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {   
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[5];
+
+FillArray(array);
+PrintArray(array);
+
+int size = array.Length;
+int i = 1;
+int result = 0;
+
+while ( i < size )
+{
+    result = array[i] + result;
+    i++;
+}
+Console.WriteLine("Cумма всех элементов массива с нечетным индексом:");
+Console.WriteLine(result);
