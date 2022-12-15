ef9d1dc0f73f24f569bfbbd837e73b534a8a00f8
Dragach <dragach@MacBook-Pro-user1.local>
2022-12-15 11:30:41 +0700

    задача 38 готова

---
+++ задача_38/Program.cs
@@ -0,0 +1,24 @@
+﻿// функция которая печатает массив
+void PrintArray( int[] col )
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {   
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+//задаем массив целых чисел от -10 до 10
+int[] array = new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+//находим разницу между максимальным и минимальным элементом массива
+int dif = array.Length - 1;
+
+//Выводим массив 
+PrintArray(array);
+Console.WriteLine(" ");
+
+//Выводим наш ответ 
+Console.WriteLine(dif);
