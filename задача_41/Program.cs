5d01c40ff89e46675f9ed6697739bc88f39fa2de
Dragach <dragach@MacBook-Pro-user1.local>
2022-12-20 16:43:57 +0700

    задача 41 готова

---
+++ задача_41/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] GetArray (string sArray)
+{
+    string[] i = sArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[i.Length];
+    for (int count = 0; count < result.Length; count++)
+    {
+        result[count] = int.Parse(i[count]);
+    }
+    return result;
+}
+
+int Elements(int[] a)
+{
+    int c = 0;
+    foreach (var item in a)
+    {
+        if (item>0) c++;
+    }
+    return c;
+}
+
+Console.Write("Введите числа через пробел:");
+
+int[] array = GetArray(Console.ReadLine()!);
+Console.WriteLine($"Колличество чисел>0: {Elements(array)}");
+
+
