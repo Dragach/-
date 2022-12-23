2c5fe46d0ef9abbaae51980c49f1caa57b822755
Dragach <dragach@MacBook-Pro-user1.local>
2022-12-23 17:40:27 +0300

    задача 47 готова

---
+++ задача_47/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] Array = new double[m, n];
+Random rnd = new Random();
+
+
+//функция создания двумерного массива 
+//со случайными вещественными числами
+void FillArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);}}}
+FillArray(Array);
+Console.WriteLine();
+PrintArray(Array);
+
+//функция вывода двумерного массива
+void PrintArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { Console.Write($"{matr[i, j]} ");}
+ Console.WriteLine();}}
+
+
+
+
+
+
